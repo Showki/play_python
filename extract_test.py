bbe16a9e9f729ab9bcd32db50f2b0f99d356cc99
Showki <ering.scissorman@gmail.com>
2016-09-22 15:28:01 +0900

    for文からwhile文へ切り替え

--- extract_test.py
+++ extract_test.py
@@ -1,4 +1,3 @@
-
 import MeCab
 mecab = MeCab.Tagger('mecabrc')
 #UnicodeDecodeError回避a)
@@ -7,9 +6,7 @@ mecab.parse('')
 def tokenize(text):
     #mecab.parse('')#文字列がGCされるのを防ぐ
     node = mecab.parseToNode(text)
-    words = {}
-    #words = collections.OrderedDict()
-    i = 0
+    words = []
     node = node.next
     while node:
         #単語を取得
@@ -17,19 +14,28 @@ def tokenize(text):
         #品詞を取得
         #品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用形,活用型,原形,読み,発音
         pos = node.feature.split(",")[0]
-        words[i] = {word : pos}
-        #次の単語に進める
+        words.append([word,pos])
         node = node.next
-        i += 1
-    #リスト末尾の'BOS/EOS'を削除
-    del words[i-1]
+    words.pop()
     return words
+
+def concatenateNoun(tokenized_text):
+    composite_noun = ''
+    for word in tokenized_text:
+        if word[1] == '名詞':
+            composite_noun += word[0]
+        else:
+
+
+    return type(tokenized_text)
 
 if __name__ == '__main__':
     sentence = "本堂と境内にいる「夫婦カツラ」が盛岡市の文化財として指定されている寺はどこですか。"
     answer = "援交寺"
-    analyzed_sentence = tokenize(sentence)
-    analyzed_answer = tokenize(answer)
-    print(analyzed_answer,analyzed_sentence)
+    result_sentence = concatenateNoun(tokenize(sentence))
+    #result_sentence = tokenize(sentence)
+    result_answer = concatenateNoun(tokenize(answer))
+    #result_answer = tokenize(answer)
+    print(result_sentence[1],result_answer[1])
 
 
